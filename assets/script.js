e78b841f7e7ede3942a686fb63634591d33e8708
BSandfer <bsandfer@gmail.com>
2022-02-24 00:33:15 -0800

    quiz ok starting high score log

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,71 @@
+const timerFunction = () => {
+    timer -= 1
+    document.getElementById('timer').innerHTML = `
+    ${timer}
+    `
+    if (timer < 0) {
+        document.getElementById('all').innerHTML = `
+        <h1> Try Again. </h1>
+        `
+    }
+}
+
+let myTimer
+
 // showing questions when we click start 
 document.getElementById("start").addEventListener('click', event => {
     document.getElementById("showMe").classList.remove('hidden')
+    displayScore()
+    myTimer = setInterval(timerFunction, 1000) 
     displayQuestion()
 
 })
+// diplay the score 
+const displayScore = () => {
+    document.getElementById("score").innerHTML = `
+    Score: ${score}
+    `
+}
 
-// console log answer selection 
+const quizFinished = () => {
+    displayScore()
+    clearInterval(myTimer)
+    console.log('quiz finished')
+    document.getElementById('myQuestion').innerHTML = ''
+    document.getElementById('myOptions').innerHTML = ''
+}
+
+// console log | answer selection 
 document.addEventListener('click', event => {
     if(event.target.classList.contains('option')) {
-        // console.log(event.target.dataset.option)
-        // console.log(event.target.dataset.answer)
+    
         if(event.target.dataset.option== event.target.dataset.answer) {
             console.log('Correct!')
             score += 1
             currentQuestion += 1
-            displayQuestion()
+            if(currentQuestion == questions.length) {
+                quizFinished()
+                displayScore()
+                clearInterval(myTimer)
+               
+
+            } else {
+                displayScore()
+                displayQuestion()
+            }
         } else {
             console.log('Wrong.')
+            timer -= 5
         }
-    
-    }
-    
+    }   
 })
+
 
 // set question number, score and timer
 
 currentQuestion = 0
 let score = 0
-let timer = 0
+let timer = 30
 
 // define variables as arrays
 let questions = [
@@ -43,6 +81,7 @@ let questions = [
     
     }
 ]
+
 
 const displayQuestion = () => {
     document.getElementById("myQuestion").innerHTML = `
