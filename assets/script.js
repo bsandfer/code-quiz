7859d11d1a8cd9dca8b44e4024b01884fc73cd42
BSandfer <bsandfer@gmail.com>
2022-02-23 21:36:18 -0800

    showing and formatting questions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,40 @@
+// showing questions when we click start 
 document.getElementById("start").addEventListener('click', event => {
-    document.getElementById("loadMe").classList.remove('hidden')
+    document.getElementById("showMe").classList.remove('hidden')
+    displayQuestion()
+
 })
+
+// question count start at zero
+currentQuestion = 0
+
+// define variables as arrays
+let questions = [
+    {
+        question: "why did the chicken cross the road?",
+        options: ['to get to the other side', 'who knows?'],
+        answer: 'to get to the other side'
+    },
+    {
+        question: "knock knock",
+        options: ['whos there?', 'who knows?'],
+        answer: 'whos there?'
+    
+    }
+]
+
+const displayQuestion = () => {
+    document.getElementById("myQuestion").innerHTML = `
+    <p>
+    ${questions[currentQuestion].question}
+    </p>
+    `
+    document.getElementById('myOptions').innerHTML = `
+    <p>
+    ${questions[currentQuestion].options[0]}
+    </p>
+    <p>
+    ${questions[currentQuestion].options[1]}
+    </p>
+    `
+}
